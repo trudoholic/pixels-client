a2c73d06246b857ebbc17e343b5aaa06afcde72a
Oleg <35769958+trudoholic@users.noreply.github.com>
2024-01-28 01:52:39 +0300

    ApolloClient

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,31 @@ import { useState } from 'react'
 import ColorPicker from "./ColorPicker.jsx"
 import Pixel from "./Pixel.jsx"
 
+//
+import {ApolloClient, HttpLink, InMemoryCache} from 'apollo-boost'
+import gql from 'graphql-tag'
+
+const endPointUrl = 'http://localhost:9000/graphql'
+const client = new ApolloClient({
+  link: new HttpLink({uri: endPointUrl}),
+  cache: new InMemoryCache(),
+})
+
+async function loadPixelsAsync() {
+  const query = gql`
+    {
+      pixels {
+        id
+        color
+      }
+    }
+  `
+  const {data: {pixels}} = await client.query({query})
+  console.log("data.pixels", pixels)
+  return pixels
+}
+//
+
 const pixels = new Array(400).fill("white")
 
 function App() {
@@ -17,6 +42,8 @@ function App() {
           <Pixel color={pixel} key={idx} newColor={color} />
         ))}
       </div>
+      <p>Apollo GraphQL</p>
+      <input type="button"  value="Load Pixels" onClick={loadPixelsAsync}/>
     </div>
   )
 }
